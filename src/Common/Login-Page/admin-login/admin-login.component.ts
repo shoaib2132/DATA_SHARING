e91b060c97910e65b0a4a523afddb8336ab23d99
priyankamalgunkar <priyanka.malgunkar@comflextech.com>
2024-09-12 10:19:55 +0530

    working on admin login component

--- src/Common/Login-Page/admin-login/admin-login.component.ts
+++ src/Common/Login-Page/admin-login/admin-login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css'
 })
 export class AdminLoginComponent {
 
+
+  constructor(private Router:Router){
+
+  }
+
+  validateLogin(){
+    this.Router.navigate(['/user/clientpage'])
+
+  }
+
+
 }
+
+
