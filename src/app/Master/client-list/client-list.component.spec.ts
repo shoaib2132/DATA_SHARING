93935c3d4d74d6ba5d20ad53605e89418175ebe9
priyankamalgunkar <priyanka.malgunkar@comflextech.com>
2024-09-17 11:32:32 +0530

    Working On Client list

--- src/app/Master/client-list/client-list.component.spec.ts
+++ src/app/Master/client-list/client-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ClientPageComponent } from './client-page.component';
+import { ClientListComponent } from './client-list.component';
 
-describe('ClientPageComponent', () => {
-  let component: ClientPageComponent;
-  let fixture: ComponentFixture<ClientPageComponent>;
+describe('ClientListComponent', () => {
+  let component: ClientListComponent;
+  let fixture: ComponentFixture<ClientListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClientPageComponent]
+      imports: [ClientListComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ClientPageComponent);
+    fixture = TestBed.createComponent(ClientListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
