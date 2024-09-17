a7c2741f109aa8d408856bc606dca8d5738c4bd1
priyankamalgunkar <priyanka.malgunkar@comflextech.com>
2024-09-17 14:42:51 +0530

    Working on login page

--- src/Common/Login-Page/user-login/user-login.component.ts
+++ src/Common/Login-Page/user-login/user-login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [InputTextModule],
+  imports: [InputTextModule,PasswordModule,FloatLabelModule,FormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
-
+  COMPANY_CODE:any;
+  EMPLOYEE_CODE:any;
+  PASSWORD:any;
+  CAPTCHA:any;
 }
